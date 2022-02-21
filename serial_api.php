e21708591ba0d748df8fed807aba575e81349f1b
Thamothiran Naturalist <29664661+thamo-nature@users.noreply.github.com>
2022-02-21 13:14:47 +0530

    Create serial_api.php

---
+++ serial_api.php
@@ -0,0 +1,16 @@
+<?php
+
+$data = json_encode($_POST);
+
+$para_meter = json_decode($data);
+
+print_r($para_meter);
+
+//access varisable by
+$one = $parameter->inc_co2;
+$tow = $parametr->inc_temp;
+
+#handle db as you wish /...../
+ 
+  
+?>
